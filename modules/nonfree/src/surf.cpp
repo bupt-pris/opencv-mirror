bupt-pris/opencv-mirror

commit 185e97d6febc131ea74fa542b8f24e919dded023

fixed angles of SURF detector
git-svn-id: http://code.opencv.org/svn/opencv/trunk/opencv@8892 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

modules/nonfree/src/surf.cpp
@@ -667,7 +667,7 @@ struct SURFInvoker
                         besty = sumy;
                     }
                 }
-                descriptor_dir = fastAtan2( besty, bestx );
+                descriptor_dir = fastAtan2( -besty, bestx );
             }
             kp.angle = descriptor_dir;
             if( !descriptors || !descriptors->data )

commit b2e4674bd49f9b605f32afac570a3488be31903d

fixed SURF orientations, broken in r8892 (see also r8895, r8887 and #2043)
git-svn-id: http://code.opencv.org/svn/opencv/trunk/opencv@9002 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

modules/nonfree/src/surf.cpp
@@ -681,8 +681,8 @@ struct SURFInvoker
             if( !upright )
             {
                 descriptor_dir *= (float)(CV_PI/180);
-                float sin_dir = std::sin(descriptor_dir);
-                float cos_dir = std::cos(descriptor_dir);
+                float sin_dir = -std::sin(descriptor_dir);
+                float cos_dir =  std::cos(descriptor_dir);
 
                 /* Subpixel interpolation version (slower). Subpixel not required since
                 the pixels will all get averaged when we scale down to 20 pixels */